ebd54552d4db45e030d27a7a1a78884c23be4bd0
Keerati <keerati.jear@gmail.com>
2019-04-17 02:50:21 +0700

    [ADD] add timer app with core function start, stop and reset !

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import './App.css';
-import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react';
 
 import { observer } from 'mobx-react'
 
-class App extends Component {
-  render() {
-    const { timer } = this.props
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            {timer.count}
-          </p>
-          <button onClick={() => timer.increment()}>+</button>
-          <button onClick={() => timer.decrement()}>-</button>
-        </header>
-      </div>
-    );
-  }
+function isCreator(permission) {
+  return permission === 'creator'
 }
+
+const App = ({ timer }) => {
+  const { time, permission, room } = timer
+
+  // useEffect(() => {
+    // const permission = localStorage.getItem('permission')
+    // if (permission) {
+      // timer.setPermission(permission)
+    // }
+  // })
+
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <p className='mt-5'>
+              You are <u>"{permission}"</u> of {room} room.
+            </p>
+            <h1>{`${time.format('HH:mm:ss')}`}</h1>
+            {
+              isCreator(permission) && (
+                <React.Fragment>
+                  <div className="mt-5 d-flex justify-content-center align-items-center">
+                    <input
+                      min='0'
+                      max='23'
+                      onChange={e => timer.setHour(+e.target.value)}
+                      value={time.hour()}
+                      type='number'
+                      className='form-control'
+                    />:
+                    <input
+                      min='0'
+                      max='59'
+                      onChange={e => timer.setMinute(+e.target.value)}
+                      value={time.minute()}
+                      type='number'
+                      className='form-control'
+                    />:
+                    <input
+                      min='0'
+                      max='59'
+                      onChange={e => timer.setSecond(+e.target.value)}
+                      value={time.second()}
+                      type='number'
+                      className='form-control'
+                    />
+                  </div>
+                  <div className="mt-5 d-flex justify-content-between">
+                    <button className='btn btn-success' onClick={() => timer.startTimer()}>Start</button>
+                    <button className='btn btn-danger' onClick={() => timer.stopTimer()}>Stop</button>
+                    <button className='btn btn-warning' onClick={() => timer.reset()}>Reset</button>
+                  </div>
+                </React.Fragment>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    )
+};
 
 export default observer(App);
--- src/store/Timer.js
+++ src/store/Timer.js
@@ -1,20 +1,59 @@
+import day from 'dayjs'
 import { extendObservable } from 'mobx';
 
 class Timer {
   constructor() {
     extendObservable(this, {
-        count: 0,
-        room: '',
-        timer: new Date()
+        permission: 'creator',
+        room: 'default',
+        time: day().set('hour', 0).set('minute', 3).set('second', 0),
+        backup: day().set('hour', 0).set('minute', 3).set('second', 0),
+        interval: null,
     });
   }
 
-  increment() {
-    this.count++;
+  setPermission(permission) {
+    this.permission = permission;
   }
 
-  decrement() {
-    this.count--;
+  setBackup() {
+    this.backup = this.time
+  }
+
+  setHour(hour = 0) {
+    this.time = this.time.set('hour', hour)
+    this.setBackup()
+  }
+
+
+  setMinute(minute = 0) {
+    this.time = this.time.set('minute', minute)
+    this.setBackup()
+  }
+
+  setSecond(second = 0) {
+    this.time = this.time.set('second', second)
+    this.setBackup()
+  }
+
+  startTimer() {
+    if (this.interval === null) {
+      this.interval = setInterval(() => {
+        this.time = this.time.subtract(1, 'second')
+        if (this.time.format('HH:mm:ss') === '00:00:00') {
+          this.stopTimer()
+        }
+      }, 1000);
+    }
+  }
+
+  stopTimer() {
+    clearInterval(this.interval)
+    this.interval = null
+  }
+
+  reset() {
+    this.time = this.backup
   }
 }
 
